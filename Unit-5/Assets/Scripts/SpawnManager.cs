8d9eef4a5e3a1490731ac64eb528499432933ea7
Max Field <maxfield7766@gmail.com>
2023-03-09 12:27:13 +1300

    gfhj

---
+++ Unit-5/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] objects;
+
+    float xRange = 5;
+
+    void Start()
+    {
+        InvokeRepeating("spawnObjects", 5f, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void spawnObjects()
+    {
+        int randomRange = Random.Range(-5, 5);
+        int randomObject = Random.Range(0, 3);
+
+        Instantiate(objects[randomObject], new Vector3(randomRange, -3, 0), transform.rotation);
+    }
+}
---
+++ Unit-5/Assets/Scripts/TossObjects.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TossObjects : MonoBehaviour
+{
+    Rigidbody objectRigidBody;
+
+    void Start()
+    {
+        objectRigidBody = GetComponent<Rigidbody>();
+
+        objectRigidBody.AddForce(Vector3.up * Random.RandomRange(10, 16), ForceMode.Impulse);
+        objectRigidBody.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+
+}
