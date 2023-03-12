a0fbb0ff683b4a53564d3591cc5410ac37e498ed
Max Field <maxfield7766@gmail.com>
2023-03-12 19:15:44 +1300

    fghj

--- Unit-5/Assets/Scripts/SpawnManager.cs
+++ Unit-5/Assets/Scripts/SpawnManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpawnManager : MonoBehaviour
 {
@@ -9,22 +10,28 @@ public class SpawnManager : MonoBehaviour
 
     public bool gameOver = false;
 
+    Button retryButton;
+
     // Start is called before the first frame update
     void Start()
     {
         playerLives = GameObject.Find("lives").GetComponent<playerLives>();
         InvokeRepeating("spawnObjects", 2f, 2f);
-        
+
+        retryButton = GameObject.Find("retryButton").GetComponent<Button>();
+        retryButton.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        GameObject.FindGameObjectWithTag("retryButton").SetActive(false);
+
         if (playerLives.lives < 1 && !gameOver)
         {
             CancelInvoke("spawnObjects");
-
             gameOver = true;
+            retryButton.gameObject.SetActive(true);
         }
     }
 
@@ -38,6 +45,7 @@ public class SpawnManager : MonoBehaviour
     void retry()
     {
         gameOver = false;
-
+        playerLives.lives += 3;
+        retryButton.gameObject.SetActive(false);
     }
 }
